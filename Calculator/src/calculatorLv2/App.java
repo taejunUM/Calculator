e996adae28b667983a51186c9de223b426ce6d15
taejunUM <utj0411@naver.com>
2024-11-20 12:16:05 +0900

    save

--- Calculator/src/calculatorLv2/App.java
+++ Calculator/src/calculatorLv2/App.java
@@ -47,11 +47,21 @@ public class App {
             int result = calculator.calculate(num1, symbol, num2);
             System.out.println("계산결과 : " + result);
 
+            while (true) {
+                calculator.setArrayList(result);
+                System.out.println("지금까지 저장된 결과: " + calculator.getResults());
 
-            calculator.setArrayList(result);
-            System.out.println("지금까지 저장된 결과: " + calculator.getResults());
-
-            System.out.println("처음 저장된 결과를 삭제하겠습니까? (y / n)");
+                System.out.print("처음 저장된 결과를 삭제하겠습니까? (y / n) ");
+                char choice = scanner.next().charAt(0);
+                if (choice == 'y') {
+                    calculator.removeResult();
+                    break;
+                } else if (choice == 'n') {
+                    break;
+                } else {
+                    System.out.println("잘못된 입력값입니다. 다시 입력해주세요.");
+                }
+            }
 
             System.out.print("더 계산하시려면 아무 키나 입력해주세요. (exit를 입력하면 종료됩니다.) ");
             String exit = scanner.next();
